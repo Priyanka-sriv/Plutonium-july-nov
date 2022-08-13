516f1b06ecb447fb5f2eba8929bc8ad05d07f6a3
Priyanka <priyankashreevastava6@gmail.com>
2022-08-13 22:37:49 +0530

    movies Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
@@ -13,6 +14,73 @@ router.get('/test-me', function (req, res) {
 router.get('/students', function (req, res){
     let students = ['Sabiha', 'Neha', 'Akash']
     res.send(students)
+})
+// 1.	Create an API for GET /movies that returns a list of movies.
+//  Define an array of movies in your code and return the value in response.
+router.get('/GET-movies',function(req,res){
+    let moviesList = ["Partner","Don","Holiday",]
+    res.send(moviesList)
+
+})	
+// problem 2 And 3 
+
+router.get('/GET-movies/:indexNumber',function(req,res){
+    const  moviesList=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    let findMovieIndex = req.params.indexNumber
+    if(findMovieIndex<0||findMovieIndex>=moviesList.length){
+        return res.send("index value does not exist in your array list")
+    }
+    let indexList = moviesList[findMovieIndex]
+    res.send(indexList)
+
+})
+// problem4
+router.get('/films',function(req,res){
+ 
+   const moviesArrayList= [ {
+       "id":1,
+       "name":"The Shining"
+       }, {
+        "id":2,
+        "name":"Incendies"
+      
+       }, {
+        "id":3,
+        "name":"Rang de Basanti"
+     
+       }, {
+        "id":4,
+        "name":"Finding Nemo"
+       }]
+       res.send(moviesArrayList)
+      
+})
+// problem 5
+router.get('/GET/films/:flimId',function(req,res){
+    const arrayList= [ {
+        "id":1,
+        "name":"The Shining"
+        }, {
+         "id":2,
+         "name":"Incendies"
+       
+        }, {
+         "id":3,
+         "name":"Rang de Basanti"
+      
+        }, {
+         "id":4,
+         "name":"Finding Nemo"
+        }]
+        // lets take filmid in req.params
+        let moviesId = req.params.flimId
+        for(let i=0;i<arrayList.length; i++){
+            let myFilms = arrayList[i]
+            if(myFilms.id == moviesId){
+               return res.send(myFilms)
+            }   
+        }
+        res.send("No movie exists with this id")
 })
 
 router.get('/student-details/:name', function(req, res){
