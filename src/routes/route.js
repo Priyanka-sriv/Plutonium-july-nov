b861ae2cbd68017c3b7a34154a2478719076c597
Priyanka <priyankashreevastava6@gmail.com>
2022-08-14 14:14:20 +0530

    post api

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,6 +6,67 @@ const UserController= require("../controllers/userController")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+   
+// Write a POST /players api that creates a new player 
+// ( i.e. that saves a player’s details and doesn’t allow saving the data
+//  of a player with a name that already exists in the data)
+
+   router.post('/players', function (req, res) {
+
+    //LOGIC WILL COME HERE
+    let addNewArray = req.body
+    let newPlayerName = req.body.name
+    for(let i=0;i<players.length;i++){
+        if(players[i].name == newPlayerName){
+            return res.send("does not allowed same name data")
+        }
+    }
+    let myValue = players.push(addNewArray)
+    res.send(players)
+})
+
+
+
+   
+
+
+
+
+
+
+
+
+
 
 router.post("/createUser", UserController.createUser  )
 
